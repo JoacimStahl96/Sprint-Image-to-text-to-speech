1d43c5a992164dd1cb72cc4d04361ccdf9eeeded
JoacimStahl96 <69908677+JoacimStahl96@users.noreply.github.com>
2021-06-22 13:10:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sprint_image_to_text_to_speech/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sprint_image_to_text_to_speech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
